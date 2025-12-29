350402b015766da1a554be48edd67c96e43b4a67
ImRuliad <nicholaseovva@csus.edu>
2025-12-29 15:53:30 -0800

    define model

--- notebook_two.py
+++ notebook_two.py
@@ -40,13 +40,41 @@ def create_dataloaders(dataset, batch_size, shuffle):
     print(f"Successfully created dataloader for {dataset} with BATCH SIZE {batch_size}")
     return dataloader
 
+def get_single_data_batch(dataloader):
+    x, y = next(iter(dataloader))
+    print("Image tensor shape:", x.shape)
+    print("Label tensor shape:", y.shape)
+    print(f"\nExpected image shape: (64, 1, 28, 28)")
+    print(f"Expected label shape: (64,)")
+    print(f"\nLabels in this batch: {y[:10].tolist()}...")
+
+class SimpleMLP(nn.Module):
+    def __init__(self):
+        super(SimpleMLP, self).__init__()
+        self.flatten = nn.Flatten()             #Flatten 28x28 image to 784 
+        self.linear1 = nn.Linear(784, 128)      #First Layer: 784 -> 128
+        self.relu1 = nn.ReLU()                  
+        self.linear2 = nn.Linear(128, 64)       #Second Layer: 128 -> 64
+        self.relu2 = nn.ReLU()
+        self.linear3 = nn.Linear(64, 10)        #Output Layer: 64 -> 10 (One for each digit 0-9)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.relu1(x)
+        x = self.linear2(x)
+        x = self.relu2(x)
+        x = self.linear3(x)
+        return x
 
 if __name__ == "__main__":
     transform = transforms.ToTensor()
+    model = SimpleMLP()
     BATCH_SIZE = 64
 
     train_dataset = create_train_dataset(transform)
     test_dataset = create_test_dataset(transform)
-
-    create_dataloaders(train_dataset, BATCH_SIZE, shuffle=True)
-    create_dataloaders(test_dataset, BATCH_SIZE, shuffle=False)
+    train_dataloader = create_dataloaders(train_dataset, BATCH_SIZE, shuffle=True)
+    test_dataloader = create_dataloaders(test_dataset, BATCH_SIZE, shuffle=False)
+    
+    get_single_data_batch(train_dataloader)
